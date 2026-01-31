2c6aafa5dca21d54fc09cd6d5de9b4bec2c04080
Eric Mueller <nevinera@gmail.com>
2026-01-30 23:17:12 -0500

    Support "prior" content on the SentMessage
    It should be nil in the existing cases

--- lib/slack_line/sent_message.rb
+++ lib/slack_line/sent_message.rb
@@ -2,13 +2,14 @@ module SlackLine
   class SentMessage
     extend Forwardable
 
-    def initialize(original_content:, response:, client:)
-      @original_content = original_content
+    def initialize(response:, client:, content:, priorly: nil)
+      @content = content
+      @priorly = priorly
       @response = response
       @client = client
     end
 
-    attr_reader :original_content, :response
+    attr_reader :content, :priorly, :response
     def_delegators :response, :ts, :channel
 
     def inspect = "#<#{self.class} channel=#{channel.inspect} ts=#{ts.inspect}>"
--- spec/slack_line/sent_message_spec.rb
+++ spec/slack_line/sent_message_spec.rb
@@ -1,11 +1,18 @@
 RSpec.describe SlackLine::SentMessage do
-  let(:original_content) { [type: "section", text: {type: "mrkdwn", text: "Hello"}] }
+  let(:content) { [type: "section", text: {type: "mrkdwn", text: "Hello"}] }
+  let(:priorly) { nil }
   let(:response) { Slack::Messages::Message.new({ts: "1234567890.123456", channel: "C12345678"}) }
   let(:client) { instance_double(SlackLine::Client) }
-  subject(:sent_message) { described_class.new(original_content:, response:, client:) }
+  subject(:sent_message) { described_class.new(content:, priorly:, response:, client:) }
 
-  it { is_expected.to have_attributes(original_content:, response:) }
+  it { is_expected.to have_attributes(content:, response:, priorly: nil) }
   it { is_expected.to have_attributes(ts: "1234567890.123456", channel: "C12345678") }
+
+  context "when prior content is supplied" do
+    let(:priorly) { [type: "section", text: {type: "mrkdwn", text: "Previous message"}] }
+
+    it { is_expected.to have_attributes(content:, priorly:) }
+  end
 
   describe "#inspect" do
     subject(:inspect_output) { sent_message.inspect }
