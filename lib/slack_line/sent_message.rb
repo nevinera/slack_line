9ead5c5b45e9afffcf8b2870f42b12223661f748
Eric Mueller <nevinera@gmail.com>
2026-01-30 23:27:49 -0500

    Support SentMessage#update
    accepts the same parameters SlackLine.message does (string, strings,
    blocks object, dsl-block)

--- lib/slack_line/sent_message.rb
+++ lib/slack_line/sent_message.rb
@@ -14,8 +14,16 @@ module SlackLine
 
     def inspect = "#<#{self.class} channel=#{channel.inspect} ts=#{ts.inspect}>"
 
+    def update(*text_or_blocks, &dsl_block)
+      updated_message = Message.new(*text_or_blocks, client:, &dsl_block)
+      new_content = updated_message.content.as_json
+      response = slack_client.chat_update(channel:, ts:, blocks: new_content)
+      SentMessage.new(content: new_content, priorly: content, response:, client:)
+    end
+
     private
 
     attr_reader :client
+    def_delegators :client, :slack_client
   end
 end
