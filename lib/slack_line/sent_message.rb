be5e6e8f02cb21130f32dcc2eae1c5908cf44fdf
Eric Mueller <nevinera@gmail.com>
2026-01-30 23:03:57 -0500

    inject the Client into SentMessage

--- lib/slack_line/sent_message.rb
+++ lib/slack_line/sent_message.rb
@@ -2,14 +2,19 @@ module SlackLine
   class SentMessage
     extend Forwardable
 
-    def initialize(original_content:, response:)
+    def initialize(original_content:, response:, client:)
       @original_content = original_content
       @response = response
+      @client = client
     end
 
     attr_reader :original_content, :response
     def_delegators :response, :ts, :channel
 
     def inspect = "#<#{self.class} channel=#{channel.inspect} ts=#{ts.inspect}>"
+
+    private
+
+    attr_reader :client
   end
 end
--- spec/slack_line/sent_message_spec.rb
+++ spec/slack_line/sent_message_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe SlackLine::SentMessage do
   let(:original_content) { [type: "section", text: {type: "mrkdwn", text: "Hello"}] }
   let(:response) { Slack::Messages::Message.new({ts: "1234567890.123456", channel: "C12345678"}) }
-  subject(:sent_message) { described_class.new(original_content: original_content, response: response) }
+  let(:client) { instance_double(SlackLine::Client) }
+  subject(:sent_message) { described_class.new(original_content:, response:, client:) }
 
   it { is_expected.to have_attributes(original_content:, response:) }
   it { is_expected.to have_attributes(ts: "1234567890.123456", channel: "C12345678") }
