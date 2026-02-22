654422a03bdf420126a0f9c8d3c948468a088329
Eric Mueller <nevinera@gmail.com>
2026-02-21 19:28:31 -0500

    Add SlackLine::Cli module with ExitException

---
+++ lib/slack_line/cli.rb
@@ -0,0 +1,8 @@
+module SlackLine
+  module Cli
+    ExitException = Class.new(StandardError)
+  end
+end
+
+glob = File.expand_path("../cli/*.rb", __FILE__)
+Dir.glob(glob).sort.each { |f| require(f) }
